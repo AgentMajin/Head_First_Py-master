783abb090ba9dc2fc4b123ee0f15a43c54185c08
AgentMajin <114061260+AgentMajin@users.noreply.github.com>
2023-04-26 09:29:48 +0700

    Add files via upload

--- csv converted/Processing.py
+++ csv converted/Processing.py
@@ -1,22 +1,25 @@
-#-*-coding: utf-8-*-
+# About this program:
+## - Take .csv files in the path: D:\P.Truong\Master Data\Masterdata\SKU\SKU - Store Update\csv converted as input
+## - Categorizing file based on the file name, if HMP, or DD or TPCN exist in the file name, it would be put in equivalent category
+## - Save SKU into arrays to check later, take the SKU file with category informations and save each category
+## - Open every input files, check every SKU in each row, ignore if it's not in the category, otherwise write into an output file.
+
 import csv
 import glob
-import chardet
-
+import os
 
 # Create an array for saving all .csv file directory
-directory_path = "/Users/vuphitruong/Desktop/  /Head_First_Py-master/csv converted/"
+directory_path = "D:\\P.Truong\\Master Data\\Masterdata\SKU\\SKU - Store Update\\csv converted\\"
 filenames = glob.glob(directory_path + '/*.csv')
 
 # Create arrays for categorizing file, a file would be in at least 1 array and can be in more than 1 array
-Nganh_Hang = {'HMP':[],'DD':[],'TPCN':[],'All':[]}
-
+Category = {'HMP': [], 'DD': [], 'TPCN': [], 'All': []}
 
 # Create 3 array to categorizing SKUs:
 SKU_HMP = []
 SKU_TPCN = []
 SKU_DD = []
-with open(directory_path + 'SKU_NH-added.csv','r') as input_SKUs:
+with open(directory_path + 'SKU_NH-added.csv', 'r') as input_SKUs:
     reader = csv.DictReader(input_SKUs)
     for row in reader:
         if int(row['NH']) == 2:
@@ -29,15 +32,15 @@ with open(directory_path + 'SKU_NH-added.csv','r') as input_SKUs:
 # Categorizing Files
 for filename in filenames:
     if filename.find("HMP") != -1:
-        Nganh_Hang['HMP'].append(filename)
+        Category['HMP'].append(filename)
     if filename.find("TPCN") != -1:
-        Nganh_Hang['TPCN'].append(filename)
+        Category['TPCN'].append(filename)
     if filename.find("DD") != -1:
-        Nganh_Hang['DD'].append(filename)
+        Category['DD'].append(filename)
     if filename.find("HMP") == -1 and filename.find("TPCN") == -1 and filename.find("DD") == -1:
-        Nganh_Hang['All'].append(filename)
+        Category['All'].append(filename)
 
-Nganh_Hang['All'].remove(directory_path+'SKU_NH-added.csv')
+Category['All'].remove(directory_path + 'SKU_NH-added.csv')
 # for NH in Nganh_Hang:
 #     print(NH)
 #     for filenames in Nganh_Hang[NH]:
@@ -45,27 +48,27 @@ Nganh_Hang['All'].remove(directory_path+'SKU_NH-added.csv')
 
 
 # Open file and write an output to save result
-for category in Nganh_Hang:
-    if category == 'HMP':
+for categories in Category:
+    if categories == 'HMP':
         check_array = SKU_HMP
-    elif category == 'TPCN':
+    elif categories == 'TPCN':
         check_array = SKU_TPCN
-    elif category == 'DD':
+    elif categories == 'DD':
         check_array = SKU_DD
     else:
         check_array = SKU_DD + SKU_HMP + SKU_TPCN
-    for files in Nganh_Hang[category]:
+    for files in Category[categories]:
         # Convert filename to include Store name only. Used for output name later.
-        output = files.replace(" - HMP",'').replace(" - TPCN",'').replace(" - DD",'').replace('csv converted/','csv converted/output/')
+        output = files.replace(" - HMP", '').replace(" - TPCN", '').replace(" - DD", '').replace('csv converted\\',
+                                                                                                 'csv converted\\output\\')
         # Open input and output file
         try:
-            with open(files,'r',encoding='ISO-8859-1') as input_files, open(output,'w',newline='',encoding='ISO-8859-1') as output_files:
+            with open(files, 'r', encoding='ISO-8859-1') as input_files, open(output, 'a', newline='',
+                                                                              encoding='ISO-8859-1') as output_files:
                 reader = csv.DictReader(input_files)
-                writer = csv.DictWriter(output_files,fieldnames=reader.fieldnames)
+                writer = csv.DictWriter(output_files, fieldnames=reader.fieldnames)
                 for row in reader:
                     if row['ITEM'] in check_array:
                         writer.writerow(row)
         except IOError as err:
             print("Error in opening files" + str(err))
-
-

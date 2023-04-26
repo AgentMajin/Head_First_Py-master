51638c2e9ae84b43b218d0dab4f196c14e3b8b8b
AgentMajin <truongbucon@gmail.com>
2023-04-26 15:21:48 +0700

    - Added today.date - change the filenames to include filename only, not the file path - Auto Create new folder for saving output files

--- csv converted/nhap.py
+++ csv converted/nhap.py
@@ -1,30 +1,12 @@
-import csv
-import glob
+dict = {'A': [], 'B': [], 'C': []}
+A = [1,2,4]
+B = [2,5,7]
+C = [4,7,8]
 
-
-# Create an array for saving all .csv file directory
-directory_path = "/Users/vuphitruong/Desktop/  /Head_First_Py-master/csv converted/"
-filenames = glob.glob(directory_path + '/*.csv')
-
-# Create arrays for categorizing file, a file would be in at least 1 array and can be in more than 1 array
-Nganh_Hang = {'HMP':[],'DD':[],'TPCN':[],'All':[]}
-
-# Categorizing Files
-for filename in filenames:
-    if filename.find("HMP") != -1:
-        Nganh_Hang['HMP'].append(filename)
-    if filename.find("TPCN") != -1:
-        Nganh_Hang['TPCN'].append(filename)
-    if filename.find("DD") != -1:
-        Nganh_Hang['DD'].append(filename)
-    if filename not in Nganh_Hang:
-        Nganh_Hang['All'].append(filename)
-
-for category in Nganh_Hang:
-    for files in Nganh_Hang[category]:
-        output = files.replace(" - HMP",'').replace(" - TPCN",'').replace(" - DD",'')
-        with open(files) as input_files, open(output) as ouput_files:
-            reader = csv.DictReader(input_files)
-            writer = csv.DictWriter(output)
-            for row in reader:
-                if row['SKU']
+for letter in dict:
+    for number in letter:
+        for i in range(0, len(dict)):
+            if number in dict[letter:
+                print('Recycle')
+            else:
+                print('No')
